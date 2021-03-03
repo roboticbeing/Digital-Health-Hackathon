359a26259cff400425220e2e7f8acdec11f9cdfe
roboticbeing <jessicakrishtul@gmail.com>
2021-03-03 16:06:57 -0500

    Create PageNotFound component & add to routes

--- Digital-Health-Hackathon/src/app/app.module.ts
+++ Digital-Health-Hackathon/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
