de2a6434a9c6275667307938012e0fe0fa897073
roboticbeing <jessicakrishtul@gmail.com>
2021-03-04 17:05:52 -0500

    ugly suggestion carousel

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityComponent implements OnInit {
 
+  suggestions: Array<string> = ["Go for a walk", "Do yoga", "Meditate"]
+  currentSuggestion: number = 0;
+  buttonClicked: boolean = false;
+
+  toggleSuggestion() {
+    this.buttonClicked = true;
+    // increment currentSuggestion until we reach the end of the array, then start from 0
+    this.currentSuggestion = (this.currentSuggestion == this.suggestions.length - 1) ? 0 : this.currentSuggestion + 1;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
